ebe0a94fbfb3f9e85a9ee4a26b63a141c894b386
Jason <s947517134@163.com>
2021-02-17 10:08:06 +0800

    动态代理

---
+++ proxy/src/main/java/com/demo03/Client.java
@@ -0,0 +1,16 @@
+package com.demo03;
+
+public class Client {
+    public static void main(String[] args) {
+        //真实角色
+        Landlord landlord = new Landlord();
+
+        //代理角色
+        ProxyInvocationHandler pih = new ProxyInvocationHandler();
+        //通过调用程序处理角色来处理我们要调用的接口对象
+        pih.setRent(landlord);
+
+        Rent proxy = (Rent) pih.getProxy();
+        proxy.rent();
+    }
+}
